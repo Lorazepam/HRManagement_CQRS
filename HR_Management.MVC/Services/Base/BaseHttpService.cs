5c459fecdb8ba8fd29a8420c2ae258ecbdad66a1
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2023-07-09 23:05:45 +0430

    Implement BaseHttpService include ConvertApiExceptions and AddBearerToken Method

---
+++ HR_Management.MVC/Services/Base/BaseHttpService.cs
@@ -0,0 +1,56 @@
+﻿using HR_Management.MVC.Contracts;
+using System.Net.Http.Headers;
+
+namespace HR_Management.MVC.Services.Base
+{
+    public class BaseHttpService
+    {
+        protected readonly IClient _client;
+        protected readonly ILocalStorageService _localStorage;
+
+        public BaseHttpService(IClient client,ILocalStorageService localStorage)
+        {
+            _client = client;
+            _localStorage = localStorage;
+        }
+
+        protected Response<Guid> ConvertApiExceptions<Guid>(ApiException exception)
+        {
+            if (exception.StatusCode == 400)
+            {
+                return new Response<Guid>()
+                {
+                    Message = "400 error",
+                    ValidationErrors = exception.Response,
+                    Success = false
+                };
+            }
+            else if(exception.StatusCode == 404)
+            {
+                return new Response<Guid>()
+                {
+                    Message = "error 404 : NOtFound",
+                    ValidationErrors = exception.Response,
+                    Success = false
+                };
+            }
+            else
+            {
+                return new Response<Guid>()
+                {
+                    Message = "error 404 : NOtFound",
+                    Success = false
+                };
+            }
+        }
+
+        protected void AddBearerToken()
+        {
+            if (_localStorage.Exists("token"))
+            {
+                _client.HttpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", _localStorage.GetStorageValue<string>("token"));
+            }
+        }
+    }
+}
